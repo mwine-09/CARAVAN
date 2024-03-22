f15e659580de64c1959ed24476d79a5458e09e88
Mwine <amumpairetumuramye09@gmail.com>
2024-03-23 00:25:35 +0300

    Fixed the ios version in this version as well

---
+++ lib/models/trip.dart
@@ -0,0 +1,15 @@
+class TripDetails {
+  String driverName;
+  String location;
+  String destination;
+  int availableSeats;
+  DateTime dateTime;
+
+  TripDetails({
+    required this.driverName,
+    required this.location,
+    required this.destination,
+    required this.availableSeats,
+    required this.dateTime,
+  });
+}
