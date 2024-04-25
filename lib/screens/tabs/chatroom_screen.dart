e023031b077e052ce4b551981ec83b0f586c7785
Mwine <amumpairetumuramye09@gmail.com>
2024-04-25 13:50:50 +0300

    switched the bottom bar design

---
+++ lib/screens/tabs/chatroom_screen.dart
@@ -0,0 +1,98 @@
+import 'package:caravan/models/chat_room.dart';
+import 'package:caravan/providers/chat_provider.dart';
+import 'package:caravan/providers/user_profile.provider.dart';
+import 'package:caravan/screens/more%20screens/messaging_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class ChatListScreen extends StatelessWidget {
+  const ChatListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final userProfileProvider = Provider.of<UserProfileProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+            'Chats',
+            style: TextStyle(color: Colors.white),
+          ),
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          )),
+      body: Consumer<ChatProvider>(
+        builder: (context, chatProvider, child) {
+          if (chatProvider.chatrooms.isEmpty) {
+            chatProvider.loadChatrooms(userProfileProvider.userProfile.userID!);
+
+            if (chatProvider.chatrooms.isEmpty) {
+              return Center(
+                child: Text('No chats yet',
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold)),
+              );
+            }
+          }
+          return ListView.builder(
+            itemCount: chatProvider.chatrooms.length,
+            itemBuilder: (context, index) {
+              ChatRoom chatRoom = chatProvider.chatrooms[index];
+              return Column(
+                children: [
+                  ListTile(
+                    splashColor: Colors.grey,
+                    leading: const CircleAvatar(
+                      radius: 25,
+                      backgroundImage: AssetImage('assets/default_profile.jpg'),
+                    ),
+                    title: Text(
+                      chatRoom.title ?? 'No title',
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        if (chatRoom.lastMessageSenderID ==
+                            userProfileProvider.userProfile.userID)
+                          const Icon(Icons.done),
+                        Text(
+                          '  ${chatRoom.lastMessage}',
+                          style: const TextStyle(
+                            color: Color.fromARGB(167, 255, 255, 255),
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: Text(
+                      DateFormat('h:mm a')
+                          .format(chatRoom.lastMessageTime.toDate()),
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          color: const Color.fromARGB(255, 0, 132, 239),
+                          fontSize: 12,
+                          fontFamily: 'Roboto'),
+                    ),
+                    onTap: () {
+                      chatProvider.openChatroom(chatRoom.id);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ChatScreen(chatRoom: chatRoom),
+                        ),
+                      );
+                      // Navigate to the chat room screen
+                    },
+                  ),
+                  const Divider(
+                    indent: 80,
+                    endIndent: 20,
+                    color: Color.fromARGB(90, 158, 158, 158),
+                  )
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/tabs/main_scaffold.dart
@@ -0,0 +1,91 @@
+import 'package:caravan/screens/tabs/chatroom_screen.dart';
+import 'package:caravan/screens/tabs/history.dart';
+import 'package:caravan/screens/tabs/home.dart';
+import 'package:caravan/screens/tabs/settings.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final tabs = <Widget>[
+    const HistoryScreen(),
+    const Home(),
+    const ChatListScreen(),
+    const SettingsScreen()
+  ];
+
+  int currentTab = 1;
+
+  void goToTab(int index) {
+    setState(() {
+      currentTab = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: tabs[currentTab],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentTab,
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        selectedItemColor: const Color.fromARGB(255, 255, 255, 255),
+        unselectedItemColor: Colors.grey,
+        onTap: goToTab,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.history_outlined,
+              color: Colors.grey,
+            ),
+            activeIcon: Icon(
+              Icons.history,
+              color: Colors.white,
+            ),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: Colors.grey,
+            ),
+            activeIcon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.messenger_outline,
+              color: Colors.grey,
+            ),
+            activeIcon: Icon(
+              Icons.messenger,
+              color: Colors.white,
+            ),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.settings_outlined,
+              color: Colors.grey,
+            ),
+            activeIcon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            label: 'Settings',
+          ),
+          // Add more items as needed...
+        ],
+      ),
+    );
+  }
+}
